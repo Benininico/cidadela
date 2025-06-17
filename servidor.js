315e641d6ba72473e0e88261472dc71750abe2d3
Benício Amaro <benicio.amaro@etec.sp.gov.br>
2025-06-17 00:16:00 -0300

    Header Index.js

--- servidor.js
+++ servidor.js
@@ -1,12 +1,27 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const hostname = '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!\n');
+  // Caminho do arquivo index.html na mesma pasta do servidor.js
+  const filePath = path.join(__dirname, 'index.html');
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      // Se der erro ao ler arquivo, retorna 500
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Erro ao carregar o arquivo.');
+    } else {
+      // Envia o conteúdo do index.html com header correto
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(port, hostname, () => {
